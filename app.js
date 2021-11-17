868eaf74790866815db12bcec2fb3a5b68830714
Kim dongwon <skqorwkdal@naver.com>
2021-11-17 21:33:52 +0900

    one commit

---
+++ app.js
@@ -0,0 +1,90 @@
+//배열
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+// console.log(days[4])
+// days.push('sund')
+// console.log(days)
+
+// days.sort('sund')
+// console.log(days)
+
+//오브젝트
+// const player = {
+//   name: 'dongwon',
+//   points: 10,
+//   fat: true,
+// }
+// console.log(player)
+// player.lastname = 'kim'
+// console.log(player)
+
+// function sayHello(name, age) {
+//   console.log(name, age)
+// }
+// sayHello('kim', 28)
+
+// function plus(a, b) {
+//   console.log(a + b)
+// }
+// plus(8, 60)
+
+// function divdie(q, w) {
+//   console.log(q / w)
+// }
+
+// divdie(10, 2)
+
+// const player = {
+//   name: 'kim',
+//   sayHello: function (other) {
+//     console.log('hello ' + other + ' 만나서반가워')
+//   },
+// }
+
+// console.log(player)
+// player.sayHello('dong won')
+
+// const calculator = {
+//   add: function (a, b) {
+//     return a + b
+//   },
+//   minus: function (a, b) {
+//     return a - b
+//   },
+//   div: function (a, b) {
+//     return a / b
+//   },
+//   multi: function (a, b) {
+//     return a * b
+//   },
+//   powerof: function (a, b) {
+//     return a ** b
+//   },
+// }
+
+// const addResult = calculator.add(10, 3)
+// const minResult = calculator.minus(addResult, 2)
+// const divResult = calculator.div(min, 3)
+// const mulResult = calculator.multi(divResult, 5)
+// const pResult = calculator.powerof(mulResult, 7)
+
+// const age = 96
+
+// function calculateKrAge(ageOfForeigner) {
+//   return ageOfForeigner + 2
+// }
+// const krAge = calculateKrAge(age)
+// console.log(krAge)
+
+// const age = parseInt(prompt('How old are you?'))
+
+// if (isNaN(age) || age < 0) {
+//   console.log('Please write a real positive number')
+// } else if (age < 18) {
+//   console.log('You are too young')
+// } else if (age >= 18 && age <= 50) {
+//   console.log('you can drink')
+// } else if (age > 50 && age <= 80) {
+//   console.log('you should exercise')
+// } else if (age > 80) {
+//   console.log('you can do whatever you want.')
+// }
---
+++ app1.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
+
+function onLoginSubmit(event) {
+  event.preventDefault() //event가 원래하는 행동을 멈춰줌 새로고침을 막아줌
+  loginForm.classList.add(HIDDEN_CLASSNAME) //form 을 다시숨겨줌
+  const username = loginInput.value //loginInput.value를 username이라는 변수로 저장
+  localStorage.setItem(USERNAME_KEY, username) //username ,USERNAME_KEY 을 localStorage에 저장
+  paintGreeting(username) //paintGreeting (username)을 호출
+}
+//fun은  username이라는 인자를 하나 받고있고
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}` //h1요소안에 `Hello ${username}` 텍스트를 추가
+  greeting.classList.remove(HIDDEN_CLASSNAME) //h1요소로부터 hidden이라는 클래스를 제거
+}
+//실행되면 localStorage에서 savedUsername값을 얻으려고함
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  paintGreeting(savedUsername) //유저정보가 localStorage 으로옴
+}
+//paintGreeting을 실행하는건 화면에 텍스트가 출력하게하는게 다임
