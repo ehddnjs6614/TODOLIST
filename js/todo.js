1dd705fe73555d18bd26db64dcebb1b8fba93131
Kim dongwon <skqorwkdal@naver.com>
2021-11-18 22:32:02 +0900

    3 commit

---
+++ js/todo.js
@@ -0,0 +1,51 @@
+const toDoForm = document.getElementById('todo-form')
+const toDoInput = document.querySelector('#todo-form input')
+const toDoList = document.getElementById('todo-list')
+
+const TODOS_KEY = 'todos'
+
+let toDos = []
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteTodo(event) {
+  const li = event.target.parentElement
+  li.remove()
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement('li')
+  li.id = newTodo.id
+  const span = document.createElement('span')
+  span.innerText = newTodo.text
+  const button = document.createElement('button')
+  button.innerText = '❌'
+  button.addEventListener('click', deleteTodo)
+  li.appendChild(span)
+  li.appendChild(button)
+  toDoList.appendChild(li)
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault()
+  const newTodo = toDoInput.value //input의 현재 value를 새로운변수에 복사
+  toDoInput.value = ''
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  }
+  toDos.push(newTodoObj)
+  paintToDo(newTodoObj)
+  saveToDos(toDos)
+}
+toDoForm.addEventListener('submit', handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos)
+  toDos = parsedToDos
+  parsedToDos.forEach(paintToDo)
+}
